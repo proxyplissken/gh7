f657c40efab2fd057bd5bd10cd8769c65232c872
Jared Parker <jared.parker@gmail.com>
2018-10-13 08:22:19 -0500

    initial commit

---
+++ app/src/main/java/com/gh7/currency/InputAmount.java
@@ -0,0 +1,13 @@
+package com.gh7.currency;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputAmount extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_amount);
+    }
+}
