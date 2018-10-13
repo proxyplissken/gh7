7adc92c61875abb6b6c6f36fa50b3ef0e3ce8983
Jared Parker <jared.parker@gmail.com>
2018-10-13 11:29:44 -0500

    middle of mockup

--- app/src/main/java/com/gh7/currency/InputAmount.java
+++ app/src/main/java/com/gh7/currency/InputAmount.java
@@ -2,12 +2,34 @@ package com.gh7.currency;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class InputAmount extends Activity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_amount);
+        Button twentyDollar = findViewById(R.id.twentydollar);
+        final LinearLayout money = findViewById(R.id.money);
+
+        twentyDollar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView imageView = new ImageView(InputAmount.this);
+                imageView.setImageResource(R.drawable.twenty);
+                imageView.setLayoutParams(new LinearLayout.LayoutParams(380,175));
+                money.addView(imageView);
+
+            }
+        });
     }
+
 }
